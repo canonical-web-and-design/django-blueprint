a89ff5072b19b29cfe83744875e9cdb8266cd271
Robin Winslow <robin@robinwinslow.co.uk>
2014-09-13 12:01:15 +0100

    Basic fenchurch app (with Hello World)

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,10 +1,18 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
+from fenchurch import TemplateFinder
+from views import custom_404, custom_500
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'webapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+# Standard patterns
+urlpatterns = patterns(
+    '',
+    url(r'^(?P<template>.*)/?$', TemplateFinder.as_view()),  # Fenchurch
 )
+
+# Static files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Error handlers
+handler404 = custom_404
+handler500 = custom_500
