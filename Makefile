ceb4706b61626d00e358fc6913a967a8cfdc5432
Robin Winslow <robin@robinwinslow.co.uk>
2014-09-13 23:53:16 +0100

    Prepare for dokku by:
    - Add dj-static to requirements
    - Using Cling (from dj-static) for static files in WSGI
    - Add Procfile (make dokku-start)
    - Make targets for dokku

--- Makefile
+++ Makefile
@@ -3,3 +3,14 @@
 ##
 sass:
 	sass --style compressed --update static/css/
+
+##
+# For dokku - build sass and run gunicorn
+##
+dokku-start: sass run-gunicorn
+
+##
+# Run the gunicorn app
+##
+run-gunicorn:
+	gunicorn webapp.wsgi
--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -1,14 +1,16 @@
 """
 WSGI config for webapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapp.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
